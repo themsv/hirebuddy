916b7db7e6e224d84dc10a93ef68dac594be40df
Sachin <sachinkbcm@gmail.com>
2022-08-18 12:23:26 +0530

    theming added

---
+++ src/Theming.js
@@ -0,0 +1,23 @@
+import { createTheme } from "@mui/material";
+
+export const themeOptions = createTheme({
+  palette: {
+    type: "light",
+    primary: {
+      main: "#fe414d",
+    },
+    secondary: {
+      main: "#f50057",
+    },
+    text: {
+      primary: "#000000",
+      secondary: "#000000",
+      disabled: "#000000",
+    },
+  },
+  typography: {
+    button: {
+      fontSize: "2.9rem",
+    },
+  },
+});
